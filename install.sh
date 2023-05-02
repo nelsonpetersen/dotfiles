968d27e12dbf41fa26e0d3b367c8f834ba956a45
Jamie Thomson <jamie.thomson@hellofresh.com>
2023-05-02 18:24:55 +0100

    lots of aliases

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+echo alias gd=\'git diff\' >> ~/.bashrc
 echo alias gst=\'git status\' >> ~/.bashrc
+echo alias gco=\'git checkout\' >> ~/.bashrc
+echo alias gcb=\'git checkout -b\' >> ~/.bashrc
+echo alias gbD=\'git branch --delete --force\' >> ~/.bashrc
+echo alias glo=\'git log --oneline --decorate\' >> ~/.bashrc
+echo alias gp=\'git push\' >> ~/.bashrc
+echo alias gpf=\'git push --force-with-lease --force-if-includes\' >> ~/.bashrc
+echo alias gcm=\'git checkout master\' >> ~/.bashrc
+echo alias l=\'exa\' >> ~/.bashrc 
+echo alias la=\'exa -a\' >> ~/.bashrc 
+echo alias ll=\'exa -lah\' >> ~/.bashrc 
+echo alias ls=\'exa --color=auto\' >> ~/.bashrc
 
