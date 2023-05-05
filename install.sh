288a969f002a9e532aa2e59a6f08f6205feabf04
Jamie Thomson <jamie@jamie-thomson.net>
2023-05-05 08:43:47 +0100

    oh-my-zsh & plugins

--- install.sh
+++ install.sh
@@ -4,3 +4,8 @@ cat bashrc.additions >> ~/.bashrc
 
 cp ./.gitmessage ~
 cp ./.gitconfig ~
+
+# oh-my-zsh & plugins
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
+zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
+zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
