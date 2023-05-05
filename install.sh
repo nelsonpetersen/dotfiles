706bfe86c683d68d9dfc65a0f75eb2ac42ec0716
Jamie Thomson <jamie@jamie-thomson.net>
2023-05-05 14:18:48 +0100

    run cp in zsh

--- install.sh
+++ install.sh
@@ -10,4 +10,4 @@ wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - 
 zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
 zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
 cp ./.zshrc ~
-cp ./aliases.zsh $ZSH_CUSTOM/aliases.zsh
+zsh -c 'cp ./aliases.zsh $ZSH_CUSTOM/aliases.zsh'
