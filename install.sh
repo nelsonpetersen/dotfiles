fb325a7b851df3b605e61b0c843d8cbc5fc687b8
Jamie Thomson <jamie@jamie-thomson.net>
2023-05-05 14:32:43 +0100

    make zsh the default shell

--- install.sh
+++ install.sh
@@ -10,3 +10,6 @@ wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - 
 zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
 zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
 cp ./.zshrc ~
+
+#make zsh the default
+chsh -s $(which zsh)
