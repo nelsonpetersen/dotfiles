f704edd669767546dd4bfa9768d5c93edb53be06
Jamie Thomson <jamie@jamie-thomson.net>
2023-05-05 14:22:35 +0100

    remove copy
    kept failing with error
    
    `cp: cannot create regular file '/aliases.zsh': Permission denied`
    
    Seems that $SZH_CUSTOM is evaluated as blank. Don't know why.

--- install.sh
+++ install.sh
@@ -10,4 +10,3 @@ wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - 
 zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
 zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
 cp ./.zshrc ~
-zsh -c 'cp ./aliases.zsh $ZSH_CUSTOM/aliases.zsh'
